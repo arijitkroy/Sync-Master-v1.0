3c8dada05e58f8081862bb9a608eb82eca345003
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-12-28 01:06:39 +0530

    updates

---
+++ app/src/main/java/app/vercel/sync_master_ultimate/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.sync_master_ultimate.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
